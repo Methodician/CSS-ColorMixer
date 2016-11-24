658ab1e139d8cb36cda359dc0716a213cb654310
Methodician <jacobgjohnston@gmail.com>
2016-11-23 18:05:16 -0800

    Deployed to production

--- src/assets/firebase.config.ts
+++ src/assets/firebase.config.ts
@@ -1,12 +1,21 @@
 import { AuthProviders, AuthMethods } from 'angularFire2';
 
-
-export const firebaseConfig = {
+// TEST
+/*export const firebaseConfig = {
     apiKey: "AIzaSyA5rAiXARCh82cAjG4cfyeOgBMfgBJylpo",
     authDomain: "css-colormixer-test.firebaseapp.com",
     databaseURL: "https://css-colormixer-test.firebaseio.com",
     storageBucket: "css-colormixer-test.appspot.com",
     messagingSenderId: "518501943795"
+}*/
+
+// PROD
+export const firebaseConfig = {
+    apiKey: "AIzaSyAc4xI67Kr3-lqqsSTKXvKGsVF6flq-Zi8",
+    authDomain: "css-colormixer.firebaseapp.com",
+    databaseURL: "https://css-colormixer.firebaseio.com",
+    storageBucket: "css-colormixer.appspot.com",
+    messagingSenderId: "1057907796380"
 }
 
 export const authConfig = {
